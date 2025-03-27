bdf9436442f8d891a708b7de11f7f9dd535dd44e
lucyzmf <lucy.zmf@gmail.com>
2025-03-27 11:01:31 +0100

    feat: Add initial UI elements and basic timer functionality

--- app/src/main/java/dev/lucy/myapplication/MainActivity.kt
+++ app/src/main/java/dev/lucy/myapplication/MainActivity.kt
@@ -13,6 +13,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.os.SystemClock
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
